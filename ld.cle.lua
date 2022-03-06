184ed23dda8facce5280582d5195317b9ef3ff04
Ocawesome101 <ocawesome101@gmail.com>
2022-03-06 16:01:52 -0500

    some progress or something

--- ld.cle.lua
+++ ld.cle.lua
@@ -1,4 +1,6 @@
 -- LD.CLE - the CLE linker --
+-- This linker will look through file data for any `require`s that
+-- may be present. It will link the program against those libraries.
 
 local function read_link_requests(fd)
 end
